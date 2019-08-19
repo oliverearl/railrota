d4150b756137685527afc1090addd56e10a0cb06
oliverearl <ole4@aber.ac.uk>
2019-08-19 04:38:15 +0100

    code: role types
    Set up the seeder just for my own convenience. Would rather work with the real thing than dummy data in this instance.

--- project/app/RoleType.php
+++ project/app/RoleType.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class RoleType extends Model
 {
-    //
+
+    protected $defaultTypes = [
+        'trainee',
+        'guard',
+        'instructor',
+        'driver',
+        'blockman',
+        'fireman',
+        'cleaner',
+        'passed_fireman',
+        'passed_cleaner',
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'description'
+    ];
+
+
+    public function getDefaultTypes(): Array {
+        return $this->defaultTypes;
+    }
 }
