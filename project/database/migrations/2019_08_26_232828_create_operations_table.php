f8b587ee1da63cd4e88b3f37ba6da52029657bdb
oliverearl <ole4@aber.ac.uk>
2019-08-27 02:54:45 +0100

    code: operations model and migraiton
    This is where things are gonna start getting complicated, and fast

---
+++ project/database/migrations/2019_08_26_232828_create_operations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOperationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('operations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('date')->unique()->default(today());
+            $table->boolean('is_running')->default(true);
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('operations');
+    }
+}
