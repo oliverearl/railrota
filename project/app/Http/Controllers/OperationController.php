e9f1dd02b2e5a3e94e9654965fac3bb03d466b31
oliverearl <ole4@aber.ac.uk>
2019-08-27 02:58:36 +0100

    code: index, glance, pdf
    Custom non-CRUD routes are used for returning a more tabular view of upcoming operations, as the main index page will be rather bespoke. Later, I'm planning on adding a PDF export library to the software, and this route will allow the operation to be exported. In future, I'd consider allowing other data to be exportable too.

---
+++ project/app/Http/Controllers/OperationController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Operation;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class OperationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $operations = Operation::latest()->paginate(3);
+        return view('operation.index', compact('operations'));
+    }
+
+    public function glance()
+    {
+        $operations = Operation::latest()->paginate();
+        return view('operation.glance', compact('operations'));
+    }
+
+    public function pdf()
+    {
+        // TODO: Complete PDF functionality
+        // This is just a template for now, but will be filled out later
+
+        // $filename = Carbon::now()->format('ymd') . '_operations.pdf'
+
+        // $operations = Operations::get();
+        // $pdf = PDF::loadView('operation.pdf', $operations);
+        // $pdf->save(storage_path() . $filename);
+        // return $pdf->download($filename);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Operation  $operation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Operation $operation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Operation  $operation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Operation $operation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Operation  $operation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Operation $operation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Operation  $operation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Operation $operation)
+    {
+        //
+    }
+}
