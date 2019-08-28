396afd1445e2db791d0f1feb3bc0528227e8cece
oliverearl <ole4@aber.ac.uk>
2019-08-28 11:34:41 +0100

    code: being cutesy with a logo

--- project/resources/views/home.blade.php
+++ project/resources/views/home.blade.php
@@ -9,12 +9,13 @@
             <div class="card">
                 <div class="card-header">{{ config('app.name') }}</div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                        <div class="text-center">
+                            <img class="img-fluid img-thumbnail rounded mx-auto d-block" src="{{ @asset('images/logo.png') }}" alt="Logo">
+                            <p>Welcome to Railrota!</p>
+                            @if (config('app.env') === 'local')
+                                <p><strong>I am running in development mode!</strong></p>
+                            @endif
                         </div>
-                    @endif
-                    You are logged in!
                 </div>
             </div>
         </div>
