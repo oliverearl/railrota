809470673e06f89258604d2349fc4baac671af4a
oliverearl <ole4@aber.ac.uk>
2019-08-29 00:02:13 +0100

    code: basically all shift methods needed tweaking
    Binding is busted

--- project/app/Http/Controllers/OperationShiftController.php
+++ project/app/Http/Controllers/OperationShiftController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Operation;
 use App\OperationShift;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class OperationShiftController extends Controller
 {
@@ -29,8 +30,9 @@ class OperationShiftController extends Controller
     public function create(Operation $operation)
     {
         $this->authorize('create');
+        $data = OperationShift::getData();
 
-        return view('shift.create', compact('operation'));
+        return view('shift.create', compact('operation', 'data'));
     }
 
     /**
@@ -39,10 +41,29 @@ class OperationShiftController extends Controller
      * @param Operation $operation
      * @param \Illuminate\Http\Request $request
      * @return void
+     * @throws \Illuminate\Validation\ValidationException
+     * @throws \Illuminate\Auth\Access\AuthorizationException
      */
     public function store(Operation $operation, Request $request)
     {
-        //
+        $this->authorize('store');
+
+        $this->validate($request, [
+            'role_type_id' => 'required|integer|exists:role_types,id',
+            'user_id' => 'nullable|integer|exists:users,id',
+            'location_id' => 'nullable|integer|exists:locations,id',
+            'powered_locomotive_id' => 'nullable|integer|exists:powered_locomotives,id',
+            'steam_locomotive_id' => 'nullable|integer|exists:steam_locomotives,id',
+            'notes' => 'min:1|max:1024|string|nullable',
+        ]);
+
+        $operationShift = new OperationShift();
+        $operationShift->fill($request->all());
+        $operationShift->operation_id = $operation->id;
+        $operationShift->save();
+
+        flash()->success("{$operationShift->role_type->name} shift has been updated successfully!")->important();
+        return redirect()->route('operations.show', $operation->id);
     }
 
     /**
@@ -61,15 +82,20 @@ class OperationShiftController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param Operation $operation
-     * @param \App\OperationShift $operationShift
+     * @param $id
      * @return void
      * @throws \Illuminate\Auth\Access\AuthorizationException
      */
-    public function edit(Operation $operation, OperationShift $operationShift)
+    public function edit(Operation $operation, $id)
     {
-        $this->authorize('edit');
+        // TODO: I can't get the model binding to work right now, so I know something with my Eloquent models is janky
+        // This will do in the meanwhile.
 
-        return view ('shift.edit', compact('operation', 'operationShift'));
+        $this->authorize('edit');
+        $operationShift = OperationShift::findOrFail($id);
+        $data = OperationShift::getData();
+
+        return view('shift.edit', compact('operation', 'operationShift', 'data'));
     }
 
     /**
@@ -77,12 +103,40 @@ class OperationShiftController extends Controller
      *
      * @param Operation $operation
      * @param \Illuminate\Http\Request $request
-     * @param \App\OperationShift $operationShift
+     * @param $id
      * @return void
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     * @throws \Illuminate\Validation\ValidationException
      */
-    public function update(Operation $operation, Request $request, OperationShift $operationShift)
+    public function update(Operation $operation, Request $request, $id)
     {
-        //
+        // TODO: I really need to take this shift class back to the drawing board
+
+        $this->authorize('update');
+
+        $this->validate($request, [
+            'role_type_id' => 'required|integer|exists:role_types,id',
+            'user_id' => 'nullable|integer|exists:users,id',
+            'location_id' => 'nullable|integer|exists:locations,id',
+            'powered_locomotive_id' => 'nullable|integer|exists:powered_locomotives,id',
+            'steam_locomotive_id' => 'nullable|integer|exists:steam_locomotives,id',
+            'notes' => 'min:1|max:1024|string|nullable',
+        ]);
+
+        $operationShift = OperationShift::findOrFail($id);
+
+        // TODO: Kinda hacky but stops competencies persisting to the wrong role types
+        if (!is_null($operationShift->role_competency_id)) {
+            if ($operationShift->role_competency->role_type->id !== $request->role_type_id) {
+                $operationShift->role_competency_id = null;
+            }
+        }
+
+        $operationShift->fill($request->all());
+        $operationShift->save();
+
+        flash()->success("{$operationShift->role_type->name} shift has been updated successfully!")->important();
+        return redirect()->route('operations.show', $operation->id);
     }
 
     /**
@@ -94,10 +148,10 @@ class OperationShiftController extends Controller
      * @throws \Illuminate\Auth\Access\AuthorizationException
      * @throws \Exception
      */
-    public function destroy(Operation $operation, OperationShift $operationShift)
+    public function destroy(Operation $operation, $id)
     {
         $this->authorize('destroy');
-
+        $operationShift = OperationShift::findOrFail($id);
         $operationShift->delete();
 
         flash()->success('Shift deleted successfully!')->important();
