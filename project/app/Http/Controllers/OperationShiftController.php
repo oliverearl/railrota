2da0b5cbe621b479e6acf0bf581b33eb28f58f5f
oliverearl <ole4@aber.ac.uk>
2019-08-29 04:24:22 +0100

    code: register and deregister logic
    Pulled it out of the policies and put it in the methods because as always I have no idea what I am doing

--- project/app/Http/Controllers/OperationShiftController.php
+++ project/app/Http/Controllers/OperationShiftController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Operation;
 use App\OperationShift;
+use App\Role;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rule;
 
 class OperationShiftController extends Controller
@@ -156,6 +158,60 @@ class OperationShiftController extends Controller
 
         flash()->success('Shift deleted successfully!')->important();
 
+        return redirect()->route('operations.show', $operation->id);
+    }
+
+    public function register(Operation $operation, $id)
+    {
+        // $this->authorize('register'); // currently not using in exchange for this absolute unit
+
+        $operationShift = OperationShift::findOrFail($id);
+
+        // ADMIN IS CRUISE CONTROL FOR COOL
+        if (!Auth::user()->isAdmin()) {
+            // Check role
+            $roleCheck = Role::where([
+                ['user_id', '=', Auth::id()],
+                ['role_type_id', '=', $operationShift->role_type_id]
+            ])->first();
+            //dd($roleCheck);
+            if (is_null($roleCheck)) {
+                flash()->error('You do not have the required role type.')->important();
+                return redirect()->back();
+            } else {
+                // Check if it has a competency level
+                if (!is_null($operationShift->role_competency_id)) {
+                    if ($roleCheck->role_competency->tier < $operationShift->role_competency->tier) {
+                        flash()->error('You do not have a sufficient competency / grade tier.')->important();
+                        return redirect()->back();
+                    }
+                }
+                // Regardless of whether the requirement is null, or the tier requirement is met
+                // Check for vacancy - you shouldn't be able to register anyway, but it's for protection in any case
+                if (!is_null($operationShift->user_id) && $operationShift->user_id !== Auth::id()) {
+                    flash()->error('This shift is not vacant.')->important();
+                    return redirect()->back();
+                }
+            }
+        }
+
+        // Assuming everything was a tremendous success at this point
+        $operationShift->user_id = Auth::id();
+        $operationShift->update();
+
+        flash()->success('You have successfully volunteered for this shift.');
+        return redirect()->route('operations.show', $operation->id);
+    }
+
+    public function deregister(Operation $operation, $id)
+    {
+        $operationShift = OperationShift::findOrFail($id);
+
+        abort_unless($operationShift->user_id === Auth::id() || Auth::user()->isAdmin(), 403);
+        $operationShift->user_id = null;
+        $operationShift->update();
+
+        flash()->success('You have pulled out of this shift.');
         return redirect()->route('operations.show', $operation->id);
     }
 }
--- project/app/Policies/OperationShiftPolicy.php
+++ project/app/Policies/OperationShiftPolicy.php
@@ -71,46 +71,12 @@ class OperationShiftPolicy
 
     public function register(User $user, OperationShift $operationShift)
     {
-        $error = '';
-
-        // Admins can override regardless
-        if (Auth::id()->isAdmin()) {
-            return true;
-        }
-
-        // Do they have the required role type?
-        if ($user->role_type_id === $operationShift->role_type_id) {
-
-            // Then check for privilege levels based on tier, or if one isn't specified
-            if (is_null($operationShift->tier || $user->role_type->tier >= $operationShift->role_type->tier)) {
-                return true;
-            } else {
-                $error = 'You do not have the required competency / grade level.';
-            }
-        } else {
-            $error = 'You do not have the role type.';
-        }
-
-        // The shift must not already have a user assigned to it
-        if (!is_null($operationShift->user_id) && $operationShift->user_id !== $user->id) {
-            $error = 'This shift is not vacant.';
-        }
-
-        flash()->error($error)->important();
-        return redirect()->back();
+        //
     }
 
     public function deregister(User $user, OperationShift $operationShift)
     {
-        if (Auth::id()->isAdmin()) {
-            return true;
-        }
-
-        if ($user->id === $operationShift->user_id) {
-            return true;
-        }
-
-        return false;
+        //
     }
 
     /**
