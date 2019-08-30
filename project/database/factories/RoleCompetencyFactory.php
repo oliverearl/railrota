ecda7a0abe95c2c40e7b923405d815effaa5ba10
oliverearl <ole4@aber.ac.uk>
2019-08-30 02:12:54 +0100

    test: role competencies feature and unit
    And factory

---
+++ project/database/factories/RoleCompetencyFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\RoleCompetency;
+use Faker\Generator as Faker;
+
+$factory->define(RoleCompetency::class, function (Faker $faker) {
+    return [
+        'name' => $faker->company,
+        'description' => $faker->realText(),
+        'tier' => $faker->numberBetween(1, 10),
+        'role_type_id' => function() {
+            return factory(App\RoleType::class)->create()->id;
+        },
+    ];
+});
---
+++ project/tests/Unit/RoleCompetenciesTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class RoleCompetenciesTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    protected $roleCompetency;
+
+    /**
+     * Test setup
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->roleCompetency = factory('App\RoleCompetency')->create();
+    }
+
+    /**
+     * Checks to ensure that a parent role type of a competency can be successfully retrieved
+     */
+    public function test_a_role_type_can_be_derived_from_a_role_competency()
+    {
+        $this->assertIsInt($this->roleCompetency->role_type->id);
+        $this->assertInstanceOf(\App\RoleType::class, $this->roleCompetency->role_type);
+    }
+
+    /**
+     * The tier must be an integer between 1 and 10
+     */
+    public function test_a_role_competency_has_a_corresponding_tier_between_one_and_ten()
+    {
+        $this->assertIsInt($this->roleCompetency->tier);
+        $this->assertThat($this->roleCompetency->tier, $this->logicalAnd(
+            $this->GreaterThanOrEqual(1),
+            $this->lessThanOrEqual(10)
+        ));
+    }
+
+    /**
+     * Controller Defaults Check
+     */
+    public function test_an_array_of_default_role_competencies_for_a_controller_can_be_returned()
+    {
+        $defaults = $this->roleCompetency->getControllerDefaults();
+
+        $this->assertIsArray($defaults);
+        $this->assertNotEmpty($defaults);
+    }
+
+    /**
+     * Guard Defaults Check
+     */
+    public function test_an_array_of_default_role_competencies_for_a_guard_can_be_returned()
+    {
+        $defaults = $this->roleCompetency->getGuardDefaults();
+
+        $this->assertIsArray($defaults);
+        $this->assertNotEmpty($defaults);
+    }
+
+    /**
+     * Blockman Defaults Check
+     */
+    public function test_an_array_of_default_role_competencies_for_a_blockman_can_be_returned()
+    {
+        $defaults = $this->roleCompetency->getBlockmanDefaults();
+
+        $this->assertIsArray($defaults);
+        $this->assertNotEmpty($defaults);
+    }
+
+    /**
+     * Powered Locomotive Defaults Check
+     */
+    public function test_an_array_of_default_role_competencies_for_a_powered_locomotive_can_be_returned()
+    {
+        $defaults = $this->roleCompetency->getPoweredDefaults();
+
+        $this->assertIsArray($defaults);
+        $this->assertNotEmpty($defaults);
+    }
+
+    /**
+     * Steam Locomotive Defaults Check
+     */
+    public function test_an_array_of_default_role_competencies_for_a_steam_locomotive_can_be_returned()
+    {
+        $defaults = $this->roleCompetency->getSteamDefaults();
+
+        $this->assertIsArray($defaults);
+        $this->assertNotEmpty($defaults);
+    }
+}
