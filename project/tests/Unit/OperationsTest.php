32ceee7eee2bf026263b760892a187c592e3f87a
oliverearl <ole4@aber.ac.uk>
2019-08-30 10:28:11 +0100

    test: additional test for pdf files

--- project/tests/Unit/OperationsTest.php
+++ project/tests/Unit/OperationsTest.php
@@ -23,11 +23,26 @@ class OperationsTest extends TestCase
         $this->user = factory('App\User')->create();
     }
 
+    /**
+     * Operation can retrieve its children shifts
+     */
     public function test_an_operation_can_retrieve_its_associated_operation_shifts()
     {
         $this->actingAs($this->user);
         $operation = factory('App\Operation')->create();
 
         $this->assertNotNull($operation->operation_shifts);
+    }
+
+    /**
+     * Test to see whether a PDF file is created in storage when the calendar is exported using the PDF route
+     */
+    public function test_an_operation_creates_a_pdf_file_in_laravel_storage_when_the_calendar_view_is_exported()
+    {
+        $this->actingAs($this->user);
+        $this->get(route('operations.pdf'));
+        $filename = Carbon::now()->format('ymd') . '_operations.pdf';
+
+        $this->assertTrue(file_exists(storage_path() . '/' . $filename));
     }
 }
